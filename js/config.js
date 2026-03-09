8794255952c87f38610faa559e3eee87f1b9e701
pacificbeachresortw <pacificbeachresort.tw@gmail.com>
2026-03-10 00:50:34 +0800

    config: update public jsonbin bin id

--- js/config.js
+++ js/config.js
@@ -1,7 +1,4 @@
-/* TNC NEWS - Public Config
- * 此檔案由後台自動更新，存放前台讀取新聞所需的公開設定
- * Bin ID 不是機密資訊，API Key 不會存於此處
- */
+/* TNC NEWS - Public Config - auto-generated */
 window.TNC_CONFIG = {
-  jsonbinBinId: ''
+  jsonbinBinId: '69aefa4d43b1c97be9c5a9d6'
 };
